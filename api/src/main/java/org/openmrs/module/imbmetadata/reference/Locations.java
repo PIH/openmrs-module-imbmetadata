529cd927631081e4834018b359167022dff12569
mseaton <mseaton@pih.org>
2015-01-31 06:03:11 -0500

    Some initial constants and bundle packages to get us started

---
+++ api/src/main/java/org/openmrs/module/imbmetadata/reference/Locations.java
@@ -0,0 +1,46 @@
+package org.openmrs.module.imbmetadata.reference;
+
+/**
+ * Defines all of the Locations needed for the system
+ */
+public class Locations {
+
+	// Rwink Active
+	public static final String BUKORA_HEALTH_CENTER = "777984f1-fbee-4de7-aadf-39ac36a89680";
+	public static final String CYARUBARE_HEALTH_CENTER = "885f137a-7ca4-4996-b111-4c76fe1ccb38";
+	public static final String GAHARA_HEALTH_CENTER = "8f6d0fd0-8af9-49c3-bfb1-f6fc608ed040";
+	public static final String GASHONGORA_HEALTH_CENTER = "bd131526-775d-4021-9f11-e3cb3ec225b5";
+	public static final String KABARONDO_HEALTH_CENTER = "39619783-d4ae-4b08-9ef3-a35bea553c3e";
+	public static final String KABUYE_HEALTH_CENTER = "9ce5d017-e724-4576-9e2b-c71a2287bc41";
+	public static final String KARAMA_HEALTH_CENTER = "1460de5b-566e-4ea4-a0d9-8312b0a2ce22";
+	public static final String KIBUNGO_PRISON = "3f77de86-20cf-4b70-821d-44649530f97c";
+	public static final String KIREHE_HEALTH_CENTER = "a2e189e2-26fe-102b-80cb-0017a47871b2";
+	public static final String KIREHE_HOSPITAL = "d79987bd-36e1-41da-b53d-225b1ee7cdd8";
+	public static final String MAHAMA_HEALTH_CENTER = "8556eb53-11ab-4423-9659-a6c859b39121";
+	public static final String MULINDI_HEALTH_CENTER = "a2e185c8-26fe-102b-80cb-0017a47871b2";
+	public static final String MUSAZA_HEALTH_CENTER = "431e715c-b98e-4eae-819b-5f94b147fced";
+	public static final String MUSHIKIRI_HEALTH_CENTE = "b5bbea30-ce75-48e2-b3d6-6c7153d33b01";
+	public static final String NASHO_HEALTH_CENTER = "caf49ac0-53b6-4191-9267-dbf48df7eac2";
+	public static final String NDEGO_HEALTH_CENTER = "a2e19a9a-26fe-102b-80cb-0017a47871b2";
+	public static final String NSINDA_PRISON = "87c9b99f-4143-4c2d-a1a1-96af8d19f7b9";
+	public static final String NTARUKA_HEALTH_CENTER = "e9fdcfce-4ee6-44af-8406-640d07c2a3aa";
+	public static final String NYABITARE_HEALTH_CENTER = "bcdea72e-9dcb-4a7b-a072-4f766ee54e9d";
+	public static final String NYAMIRAMA_HEALTH_CENTER = "7fe8333e-c4e0-46c1-b92d-51cd865e1710";
+	public static final String NYARUBUYE_HEALTH_CENTER = "a2e181c2-26fe-102b-80cb-0017a47871b2";
+	public static final String RUKIRA_HEALTH_CENTER = "a2e187f8-26fe-102b-80cb-0017a47871b2";
+	public static final String RURAMIRA_HEALTH_CENTER = "a30b5a9e-b614-49fd-a69c-38eb05042acd";
+	public static final String RUSUMO_HEALTH_CENTER = "a2e19072-26fe-102b-80cb-0017a47871b2";
+	public static final String RUTARE_HEALTH_CENTER = "752d59fb-4b72-47b1-bc13-b03fe513dd8d";
+	public static final String RWANTONDE_HEALTH_CENTER = "1a341da9-6827-4bb2-8047-4324b904bd4a";
+	public static final String RWINKWAVU_HEALTH_CENTER = "a2e183d4-26fe-102b-80cb-0017a47871b2";
+	public static final String RWINKWAVU_HOSPITAL = "38c4b94b-97a6-4f2c-bca6-c28a37cf60ed";
+	public static final String UNKNOWN = "8d6c993e-c2cc-11de-8d13-0010c6dffd0f";
+
+	// Rwink Retired
+	public static final String RETIRED_BURERA = "e43f9620-3362-4134-adcd-cab15afad649";
+	public static final String RETIRED_DELETE = "873605f2-ce91-46a1-969c-739342bf1b1d";
+	public static final String RETIRED_GASIZA = "a2e19892-26fe-102b-80cb-0017a47871b2";
+	public static final String RETIRED_RUHENGERI = "a2e1946e-26fe-102b-80cb-0017a47871b2";
+	public static final String RETIRED_RWANDA = "a2e1925c-26fe-102b-80cb-0017a47871b2";
+	public static final String RETIRED_TEST_LOCATION = "2f72a28a-1379-4eca-928e-7732fa624282";
+}
