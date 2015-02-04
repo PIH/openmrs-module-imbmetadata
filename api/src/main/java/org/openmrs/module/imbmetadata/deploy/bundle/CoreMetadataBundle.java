c4ac2cc621e08925561edbd080732e10fd94df29
mseaton <mseaton@pih.org>
2015-02-04 02:57:13 -0500

    Move constants out of java files and into json files

--- api/src/main/java/org/openmrs/module/imbmetadata/deploy/bundle/CoreMetadataBundle.java
+++ api/src/main/java/org/openmrs/module/imbmetadata/deploy/bundle/CoreMetadataBundle.java
@@ -62,45 +62,42 @@ public class CoreMetadataBundle extends ImbMetadataBundle {
 
 		// TODO: Review and set location tags appropriately for all below locations
 
-		install(location("Unknown Location", "Unknown Location", Locations.UNKNOWN, null, allTags));
-        install(location("Rwikwavu Hospital","Rwikwavu Hospital", Locations.RWINKWAVU_HOSPITAL,null,allTags));
-        install(location("Rwikwavu Health Center","Rwikwavu Health Center", Locations.RWINKWAVU_HEALTH_CENTER,null,allTags));
-        install(location("Rwantonde Health Center","Rwantonde Health Center", Locations.RWANTONDE_HEALTH_CENTER,null,allTags));
-        install(location("Rutare Health Center","Rutare Health Center", Locations.RUTARE_HEALTH_CENTER,null,allTags));
-        install(location("Rusumo Health Center","Rusumo Health Center", Locations.RUSUMO_HEALTH_CENTER,null,allTags));
-        install(location("Ruramira Health Center","Ruramira Health Center", Locations.RURAMIRA_HEALTH_CENTER,null,allTags));
-        install(location("Rukira Health Center","Rukira Health Center", Locations.RUKIRA_HEALTH_CENTER,null,allTags));
-        install(location("Nyarubuye Health Center","Nyarubuye Health Center", Locations.NYARUBUYE_HEALTH_CENTER,null,allTags));
-        install(location("Nyamirama Health Center","Nyamirama Health Center", Locations.NYAMIRAMA_HEALTH_CENTER,null,allTags));
-        install(location("Nyabitare Health Center","Nyabitare Health Center", Locations.NYABITARE_HEALTH_CENTER,null,allTags));
-        install(location("Ntaruka Health Center","Ntaruka Health Center", Locations.NTARUKA_HEALTH_CENTER,null,allTags));
-        install(location("Nsinda Prison","Nsinda Prison", Locations.NSINDA_PRISON,null,allTags));
-        install(location("Ndego Health Center","Ndego Health Center", Locations.NDEGO_HEALTH_CENTER,null,allTags));
-        install(location("Nasho Health Center","Nasho Health Center", Locations.NASHO_HEALTH_CENTER,null,allTags));
-        install(location("Mushikiri Health Center","Mushikiri Health Center", Locations.MUSHIKIRI_HEALTH_CENTE,null,allTags));
-        install(location("Musaza Health Center","Musaza Health Center", Locations.MUSAZA_HEALTH_CENTER,null,allTags));
-        install(location("Mulindi Health Center","Mulindi Health Center", Locations.MULINDI_HEALTH_CENTER,null,allTags));
-        install(location("Mahama Health Center","Mahama Health Center", Locations.MAHAMA_HEALTH_CENTER,null,allTags));
-        install(location("Kirehe Hospital","Kirehe Hospital", Locations.KIREHE_HOSPITAL,null,allTags));
-        install(location("Kirehe Health Center","Kirehe Health Center", Locations.KIREHE_HEALTH_CENTER,null,allTags));
-        install(location("Kibungo Hospital","Kibungo Hospital", Locations.KIBUNGO_PRISON,null,allTags));
-        install(location("Karama Health Center","Karama Health Center", Locations.KARAMA_HEALTH_CENTER,null,allTags));
-        install(location("Kabuye Health Center","Kabuye Health Center", Locations.KABUYE_HEALTH_CENTER,null,allTags));
-        install(location("Kabarondo Health Center","Kabarondo Health Center", Locations.KABARONDO_HEALTH_CENTER,null,allTags));
-        install(location("Gashongora Health Center","Gashongora Health Center", Locations.GASHONGORA_HEALTH_CENTER,null,allTags));
-        install(location("Gahara Health Center","Gahara Health Center", Locations.GAHARA_HEALTH_CENTER,null,allTags));
-        install(location("Cyarubare Health Center","Cyarubare Health Center", Locations.CYARUBARE_HEALTH_CENTER,null,allTags));
-        install(location("Bukora Health Center","Bukora Health Center", Locations.BUKORA_HEALTH_CENTER,null,allTags));
+		// Rwink active locations
+		install(location("Unknown Location", "Unknown Location", Locations.UNKNOWN.UUID, null, allTags));
+        install(location("Rwikwavu Hospital","Rwikwavu Hospital", Locations.RWINKWAVU_HOSPITAL.UUID, null, allTags));
+        install(location("Rwikwavu Health Center","Rwikwavu Health Center", Locations.RWINKWAVU_HEALTH_CENTER.UUID, null, allTags));
+        install(location("Rwantonde Health Center","Rwantonde Health Center", Locations.RWANTONDE_HEALTH_CENTER.UUID, null, allTags));
+        install(location("Rutare Health Center","Rutare Health Center", Locations.RUTARE_HEALTH_CENTER.UUID, null, allTags));
+        install(location("Rusumo Health Center","Rusumo Health Center", Locations.RUSUMO_HEALTH_CENTER.UUID, null, allTags));
+        install(location("Ruramira Health Center","Ruramira Health Center", Locations.RURAMIRA_HEALTH_CENTER.UUID, null, allTags));
+        install(location("Rukira Health Center","Rukira Health Center", Locations.RUKIRA_HEALTH_CENTER.UUID, null, allTags));
+        install(location("Nyarubuye Health Center","Nyarubuye Health Center", Locations.NYARUBUYE_HEALTH_CENTER.UUID, null, allTags));
+        install(location("Nyamirama Health Center","Nyamirama Health Center", Locations.NYAMIRAMA_HEALTH_CENTER.UUID, null, allTags));
+        install(location("Nyabitare Health Center","Nyabitare Health Center", Locations.NYABITARE_HEALTH_CENTER.UUID, null, allTags));
+        install(location("Ntaruka Health Center","Ntaruka Health Center", Locations.NTARUKA_HEALTH_CENTER.UUID, null, allTags));
+        install(location("Nsinda Prison","Nsinda Prison", Locations.NSINDA_PRISON.UUID, null, allTags));
+        install(location("Ndego Health Center","Ndego Health Center", Locations.NDEGO_HEALTH_CENTER.UUID, null, allTags));
+        install(location("Nasho Health Center","Nasho Health Center", Locations.NASHO_HEALTH_CENTER.UUID, null, allTags));
+        install(location("Mushikiri Health Center","Mushikiri Health Center", Locations.MUSHIKIRI_HEALTH_CENTE.UUID, null, allTags));
+        install(location("Musaza Health Center","Musaza Health Center", Locations.MUSAZA_HEALTH_CENTER.UUID, null, allTags));
+        install(location("Mulindi Health Center","Mulindi Health Center", Locations.MULINDI_HEALTH_CENTER.UUID, null, allTags));
+        install(location("Mahama Health Center","Mahama Health Center", Locations.MAHAMA_HEALTH_CENTER.UUID, null, allTags));
+        install(location("Kirehe Hospital","Kirehe Hospital", Locations.KIREHE_HOSPITAL.UUID, null, allTags));
+        install(location("Kirehe Health Center","Kirehe Health Center", Locations.KIREHE_HEALTH_CENTER.UUID, null, allTags));
+        install(location("Kibungo Hospital","Kibungo Hospital", Locations.KIBUNGO_PRISON.UUID, null, allTags));
+        install(location("Karama Health Center","Karama Health Center", Locations.KARAMA_HEALTH_CENTER.UUID, null, allTags));
+        install(location("Kabuye Health Center","Kabuye Health Center", Locations.KABUYE_HEALTH_CENTER.UUID, null, allTags));
+        install(location("Kabarondo Health Center","Kabarondo Health Center", Locations.KABARONDO_HEALTH_CENTER.UUID, null, allTags));
+        install(location("Gashongora Health Center","Gashongora Health Center", Locations.GASHONGORA_HEALTH_CENTER.UUID, null, allTags));
+        install(location("Gahara Health Center","Gahara Health Center", Locations.GAHARA_HEALTH_CENTER.UUID, null, allTags));
+        install(location("Cyarubare Health Center","Cyarubare Health Center", Locations.CYARUBARE_HEALTH_CENTER.UUID, null, allTags));
+        install(location("Bukora Health Center","Bukora Health Center", Locations.BUKORA_HEALTH_CENTER.UUID, null, allTags));
 
         // Rwink Retired locations
-        uninstall(location("Rwanda", "Rwanda", Locations.RETIRED_RWANDA),"retired");
-        uninstall(location("Ruhengeri Health Center","Ruhengeri Health Center", Locations.RETIRED_RUHENGERI),"retired");
-        uninstall(location("Gasiza Health Center","Gasiza Health Center", Locations.RETIRED_GASIZA),"retired");
-        uninstall(location("Delete","Delete", Locations.RETIRED_DELETE),"retired");
-        uninstall(location("Burera","Burera", Locations.RETIRED_BURERA), "retired");
-
-
-        //DB Test Location
-        install(location("Inman Health Center","Inman Health Center", Locations.INMAN_HOSPITAL));
+        uninstall(location("Rwanda", "Rwanda", Locations.RETIRED_RWANDA.UUID), "retired");
+        uninstall(location("Ruhengeri Health Center","Ruhengeri Health Center", Locations.RETIRED_RUHENGERI.UUID), "retired");
+        uninstall(location("Gasiza Health Center","Gasiza Health Center", Locations.RETIRED_GASIZA.UUID), "retired");
+        uninstall(location("Delete","Delete", Locations.RETIRED_DELETE.UUID), "retired");
+        uninstall(location("Burera","Burera", Locations.RETIRED_BURERA.UUID), "retired");
 	}
 }
